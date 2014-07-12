39606dda74005254a5256d03b3b76b31396f03c3
penguinsfan77 <penguinsgaming@hotmail.ca>
2014-07-12 08:12:09 -0400

    Creative Tab
    Added a creative tab for all my items

---
+++ src/main/java/io/github/penguinsfan77/randomstuff/creativeTab/ModCreativeTab.java
@@ -0,0 +1,28 @@
+package io.github.penguinsfan77.randomstuff.creativeTab;
+
+import io.github.penguinsfan77.randomstuff.items.ModItems;
+import io.github.penguinsfan77.randomstuff.references.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ModCreativeTab {
+	
+	public static final CreativeTabs RANDOM_STUFF_TAB = new CreativeTabs(Reference.MOD_ID) {
+		
+		@Override
+		public Item getTabIconItem() {
+			
+			return ModItems.trident;
+			
+		}
+		
+		@Override
+		public String getTranslatedTabLabel() {
+			
+			return "Random Stuff";
+			
+		}
+		
+	};
+
+}
