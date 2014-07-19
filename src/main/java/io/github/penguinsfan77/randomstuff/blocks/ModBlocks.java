47e728b35d01f33f5b78a8157ee7d7283cea7475
penguinsfan77 <penguinsgaming@hotmail.ca>
2014-07-18 22:57:08 -0400

    Added an NBT utillity class

--- src/main/java/io/github/penguinsfan77/randomstuff/blocks/ModBlocks.java
+++ src/main/java/io/github/penguinsfan77/randomstuff/blocks/ModBlocks.java
@@ -6,12 +6,14 @@ public class ModBlocks {
 	
 	//Create the blocks
 	public static final ModBlock black = new Black();
+	public static final ModBlock white = new White();
 	public static final ModBlock crackedStone = new CrackedStone();
 
     public static void init() {
         
     	//Register the blocks
     	GameRegistry.registerBlock(black, "blackBlock");
+    	GameRegistry.registerBlock(white, "white_block");
     	GameRegistry.registerBlock(crackedStone, "crackedStone");
     	
     }
