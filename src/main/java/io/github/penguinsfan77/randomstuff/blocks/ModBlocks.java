b742198e2bab0f23bcbba714c43dd17c6f1f1740
penguinsfan77 <penguinsgaming@hotmail.ca>
2014-07-22 20:24:08 -0400

    Removed test blocks

--- src/main/java/io/github/penguinsfan77/randomstuff/blocks/ModBlocks.java
+++ src/main/java/io/github/penguinsfan77/randomstuff/blocks/ModBlocks.java
@@ -5,15 +5,11 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModBlocks {
 	
 	//Create the blocks
-	public static final ModBlock black = new Black();
-	public static final ModBlock white = new White();
 	public static final ModBlock crackedStone = new CrackedStone();
 
     public static void init() {
         
     	//Register the blocks
-    	GameRegistry.registerBlock(black, "blackBlock");
-    	GameRegistry.registerBlock(white, "white_block");
     	GameRegistry.registerBlock(crackedStone, "crackedStone");
     	
     }
