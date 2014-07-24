9e3a94dbc13d0b688e40013492d09322a2f6b25d
penguinsfan77 <penguinsgaming@hotmail.ca>
2014-07-24 13:34:33 -0400

    Added more cracked stone recipes

--- src/main/java/io/github/penguinsfan77/randomstuff/blocks/ModBlockRecipes.java
+++ src/main/java/io/github/penguinsfan77/randomstuff/blocks/ModBlockRecipes.java
@@ -10,7 +10,22 @@ public class ModBlockRecipes {
 	
 	public static void init() {
 		
+		//Cracked stone
 		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.crackedStone), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.stone));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.crackedStone, 2), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.stone),
+				new ItemStack(Blocks.stone));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.crackedStone, 3), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.stone),
+				new ItemStack(Blocks.stone), new ItemStack(Blocks.stone));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.crackedStone, 4), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.stone),
+				new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.crackedStone, 5), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.stone),
+				new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.crackedStone, 6), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.stone),
+				new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.crackedStone, 7), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.stone),
+				new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone));
+		GameRegistry.addShapelessRecipe(new ItemStack(ModBlocks.crackedStone, 8), new ItemStack(ModItems.hammer, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.stone),
+				new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone));
 		
 	}
 
