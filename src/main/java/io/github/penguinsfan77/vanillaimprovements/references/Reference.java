1cc69e0f9f2c40e67c95fd66c5b714340205bb85
penguinsfan77 <penguinsgaming@hotmail.ca>
2014-09-06 14:37:31 -0400

    Fixed Dev bug

--- src/main/java/io/github/penguinsfan77/vanillaimprovements/references/Reference.java
+++ src/main/java/io/github/penguinsfan77/vanillaimprovements/references/Reference.java
@@ -3,7 +3,7 @@ package io.github.penguinsfan77.vanillaimprovements.references;
 public class Reference {
 	
 	//Mod references
-	public static final String MOD_ID = "VanImp";
+	public static final String MOD_ID = "vanimp";
 	public static final String MOD_NAME = "Vanilla Improvements";
 	public static final String VERSION = "1.7.10-0.1";
 	
